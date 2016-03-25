523e52593ce509e6669e9a0e6453b1fd5e2b84c2
David Millar-Durrant <david@refme.com>
2016-03-25 19:16:51 +0000

    added new pages

--- js/MHW.js
+++ js/MHW.js
@@ -1,7 +1,15 @@
 var jumboHeight = $('.jumbotron').outerHeight();
+if(typeof heroUrl !== 'undefined')
+    $('.bg').css('background-image', "url('" + heroUrl + "')");
+var panned = 0;
 function parallax(){
     var scrolled = $(window).scrollTop();
-    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+    if(typeof mhwMap !== 'undefined'){
+        map.panBy(0 ,(panned-scrolled)/3)
+        panned = scrolled;
+    }else{
+        $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+    }
 }
 
 $(window).scroll(function(e){
